6d7793f69bf15d23d4b2b6592fbc80f5426d80d7
dineshsalunke <dineshsalunke@consultbop.com>
2024-04-06 14:54:47 +0530

    refactor: initializer for item

--- item.go
+++ item.go
@@ -8,7 +8,35 @@ type Item struct {
 	leftOrigin  *ID
 	rightOrigin *ID
 	parent      interface{}
+	parentSub   string
 	content     Content
 	info        byte
+}
+
+func newItem(
+	id *ID,
+	left *Item,
+	right *Item,
+	leftOrigin *ID,
+	rightOrigin *ID,
+	parent interface{},
+	parentSub string,
+	content Content,
+) *Item {
+	var info byte = 0
+	if content.Countable() {
+		info = BIT2
+	}
+	return &Item{
+		id:          id,
+		left:        left,
+		right:       right,
+		leftOrigin:  leftOrigin,
+		rightOrigin: rightOrigin,
+		parent:      parent,
+		parentSub:   parentSub,
+		content:     content,
+		info:        info,
+	}
 }
 
