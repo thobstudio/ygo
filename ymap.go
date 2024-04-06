3b1a015809c4eed186cd02a09ab7fc60946f0c5f
dineshsalunke <dineshsalunke@consultbop.com>
2024-04-06 14:36:48 +0530

    refactor: integrate method for ymap

--- ymap.go
+++ ymap.go
@@ -17,6 +17,7 @@ func NewYMap() *YMap {
 	return newYMap()
 }
 
-func (m *YMap) integrate(doc *Doc) {
-	m.AbstractType.integrate(doc)
+func (m *YMap) integrate(doc *Doc, item *Item) {
+	m.doc = doc
+	m.item = item
 }
