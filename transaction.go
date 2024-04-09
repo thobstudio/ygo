1ad1889d751085f553ab46e1d198bc52aecf92ec
dineshsalunke <dineshsalunke@consultbop.com>
2024-04-09 21:16:05 +0530

    refactor: add some more fields to the transaction

--- transaction.go
+++ transaction.go
@@ -5,25 +5,28 @@ type Transaction struct {
 	origin         interface{}
 	local          bool
 	deleteSet      *DeleteSet
+	changed        map[SharedType]map[string]bool // map[string]bool is like a set
 	subdocsAdded   map[*Doc]bool
 	subdocsRemoved map[*Doc]bool
 	subdocsLoaded  map[*Doc]bool
 	beforeState    map[uint32]uint32
 	afterState     map[uint32]uint32
+	mergeStructs   []any
 }
 
 func newTrasaction(doc *Doc, origin interface{}, local bool) *Transaction {
 	return &Transaction{
-		doc:         doc,
-		deleteSet:   newDeleteSet(),
-		beforeState: doc.store.StateVector(),
-		afterState:  make(map[uint32]uint32, 0),
-		//
+		doc:            doc,
+		deleteSet:      newDeleteSet(),
+		beforeState:    doc.store.StateVector(),
+		afterState:     make(map[uint32]uint32, 0),
+		changed:        make(map[SharedType]map[string]bool),
 		origin:         origin,
 		local:          local,
 		subdocsAdded:   make(map[*Doc]bool, 0),
 		subdocsRemoved: make(map[*Doc]bool, 0),
 		subdocsLoaded:  make(map[*Doc]bool, 0),
+		mergeStructs:   make([]any, 0),
 	}
 }
 
