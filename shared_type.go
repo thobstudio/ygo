5377b5824a88b7df80911f3ad03079c914ca2701
dineshsalunke <dineshsalunke@consultbop.com>
2024-04-14 12:50:18 +0530

    refactor: add more methods to the sharedtype

--- shared_type.go
+++ shared_type.go
@@ -2,6 +2,12 @@ package ynotgo
 
 type SharedType interface {
 	Item() *Item
+	Start() *Item
+	SetStart(item *Item)
+	Length() uint32
+	SetLength(length uint32)
+	GetItem(key string) *Item
+	SetItem(key string, item *Item)
 	Integrate(doc *Doc, item *Item)
 	Copy() SharedType
 	Clone() SharedType
