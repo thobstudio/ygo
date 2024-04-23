bc548a008ec46618d136c2391542d2f6d39e4ee1
dineshsalunke <dineshsalunke@consultbop.com>
2024-04-23 21:56:13 +0530

    refactor: bare minimum lib0 encoding

--- lib0/encoding.go
+++ lib0/encoding.go
@@ -5,155 +5,201 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"math"
 	"reflect"
-	"unsafe"
 )
 
-func WriteVarUint(writter *bufio.Writer, num uint32) error {
-	for num > Bits7 {
-		if err := writter.WriteByte((byte)(Bit8 | (Bits7 & num))); err != nil {
+func WriteVarUint(writer *bufio.Writer, x uint64) error {
+	for x >= 0x80 {
+		if err := writer.WriteByte(byte(x) | 0x80); err != nil {
 			return err
 		}
-		num >>= 7
+		x >>= 7
 	}
-	return writter.WriteByte((byte)(Bits7 & num))
-}
-
-func WriteVarUint8Array(writter *bufio.Writer, buf []byte) error {
-	if err := WriteVarUint(writter, uint32(len(buf))); err != nil {
-		return err
-	}
-	if _, err := writter.Write(buf); err != nil {
-		return err
+	if err := writer.WriteByte(byte(x)); err != nil {
+		return nil
 	}
 	return nil
 }
 
-func WriteVarString(writter *bufio.Writer, str string) error {
-	buf := []byte(str)
-	return WriteVarUint8Array(writter, buf)
+func WriteVarint(writer *bufio.Writer, x int32) error {
+	isNegative := x < 0
+	if isNegative {
+		x = -x
+	}
+
+	cr := int32(0)
+	if x > int32(Bits6) {
+		cr = int32(Bit8)
+	}
+	neg := int32(0)
+	if isNegative {
+		neg = int32(Bit7)
+	}
+	if err := writer.WriteByte(byte(cr | neg | (int32(Bits6) & x))); err != nil {
+		return err
+	}
+	x >>= 6
+	for x > 0 {
+		pre := int32(0)
+		if x > int32(Bits7) {
+			pre = int32(Bit8)
+		}
+		if err := writer.WriteByte(byte(pre | (int32(Bits7) & x))); err != nil {
+			return err
+		}
+		x >>= 7
+	}
+
+	return nil
 }
 
-func WriteVarInt(writer *bufio.Writer, num int, treatZeroAsNegative bool) {
-	isNegative := treatZeroAsNegative
-	if num < 0 {
-		isNegative = true
+func WriteUint8Array(writer *bufio.Writer, buf []byte) error {
+	length := len(buf)
+	if err := WriteVarUint(writer, uint64(length)); err != nil {
+		return err
 	}
-	if isNegative {
-		num = -num
-	}
-
-	// |   whether to continue reading   |         is negative         | value.
-	var tmpA uint32
-	var tmpB uint32
-	if uint32(num) > Bits6 {
-		tmpA = Bit8
-	}
-	if isNegative {
-		tmpB = Bit7
-	}
-
-	// |   whether to continue reading   |         is negative         | value.
-	writer.WriteByte((byte)(tmpA | tmpB | Bit6&uint32(num)))
-	num >>= 6
-
-	// We don't need to consider the case of num == 0 so we can use a different pattern here than above.
-	var tmpC uint32
-	if uint32(num) > Bits7 {
-		tmpC = Bit8
-	}
-	for num > 0 {
-		writer.WriteByte((byte)(tmpC | (Bits7 & uint32(num))))
-		num >>= 7
-	}
+	_, err := writer.Write(buf)
+	return err
 }
 
-func WriteAny(writter *bufio.Writer, o any) error {
+func WriteVarString(writer *bufio.Writer, s string) error {
+	length := len(s)
+	if err := WriteVarUint(writer, uint64(length)); err != nil {
+		return err
+	}
+	_, err := writer.Write([]byte(s))
+	return err
+}
+
+func WriteFloat32(writer *bufio.Writer, f float32) error {
+	return binary.Write(writer, binary.BigEndian, f)
+}
+
+func WriteFloat64(writer *bufio.Writer, f float64) error {
+	return binary.Write(writer, binary.BigEndian, f)
+}
+
+/**
+ * Encode data with efficient binary format.
+ *
+ * Differences to JSON:
+ * • Transforms data to a binary format (not to a string)
+ * • Encodes undefined, NaN, and ArrayBuffer (these can't be represented in JSON)
+ * • Numbers are efficiently encoded either as a variable length integer, as a
+ *   32 bit float, as a 64 bit float, or as a 64 bit bigint.
+ *
+ * Encoding table:
+ *
+ * | Data Type           | Prefix   | Encoding Method    | Comment |
+ * | ------------------- | -------- | ------------------ | ------- |
+ * | undefined           | 127      |                    | Functions, symbol, and everything that cannot be identified is encoded as undefined |
+ * | null                | 126      |                    | |
+ * | integer             | 125      | writeVarInt        | Only encodes 32 bit signed integers |
+ * | float32             | 124      | writeFloat32       | |
+ * | float64             | 123      | writeFloat64       | |
+ * | bigint              | 122      | writeBigInt64      | |
+ * | boolean (false)     | 121      |                    | True and false are different data types so we save the following byte |
+ * | boolean (true)      | 120      |                    | - 0b01111000 so the last bit determines whether true or false |
+ * | string              | 119      | writeVarString     | |
+ * | object<string,any>  | 118      | custom             | Writes {length} then {length} key-value pairs |
+ * | array<any>          | 117      | custom             | Writes {length} then {length} json values |
+ * | Uint8Array          | 116      | writeVarUint8Array | We use Uint8Array for any kind of binary data |
+ *
+ * Reasons for the decreasing prefix:
+ * We need the first bit for extendability (later we may want to encode the
+ * prefix with writeVarUint). The remaining 7 bits are divided as follows:
+ * [0-30]   the beginning of the data range is used for custom purposes
+ *          (defined by the function that uses this library)
+ * [31-127] the end of the data range is used for data encoding by
+ *          lib0/encoding.js
+ *
+ * @param {Encoder} encoder
+ * @param {undefined|null|number|bigint|boolean|string|Object<string,any>|Array<any>|Uint8Array} data
+ */
+func WriteAny(writer *bufio.Writer, o any) error {
 	switch t := o.(type) {
-	case string:
-		if err := writter.WriteByte(119); err != nil {
+	// 127
+	// 126
+	case nil:
+		if err := writer.WriteByte(126); err != nil {
 			return err
 		}
-		if err := WriteVarString(writter, t); err != nil {
+	// 125
+	case int:
+		if err := writer.WriteByte(125); err != nil {
 			return err
 		}
+		return WriteVarint(writer, int32(t))
+	case int64:
+		if err := writer.WriteByte(125); err != nil {
+			return err
+		}
+		return WriteVarint(writer, int32(t))
+		// 124
+	case float32:
+		if err := writer.WriteByte(124); err != nil {
+			return err
+		}
+		return WriteFloat32(writer, t)
+		// 123
+	case float64:
+		if err := writer.WriteByte(123); err != nil {
+			return err
+		}
+		return WriteFloat64(writer, t)
+		// 122 We dont handle this case
+		// 121 false
+		// 120 true
 	case bool:
-		var b uint32 = 121
+		var b byte = 121
 		if t {
 			b = 120
 		}
-		if err := writter.WriteByte((byte)(b)); err != nil {
+		writer.WriteByte(b)
+		// 119
+	case string:
+		if err := writer.WriteByte(119); err != nil {
 			return err
 		}
-	case float64: // TYPE 123: FLOAT64
-		bits := math.Float64bits(t)
-		bytes := make([]byte, 8)
-		binary.LittleEndian.PutUint64(bytes, bits)
-		if !IsLittleEndian() {
-			bytes = Reverse(bytes)
+		return WriteVarString(writer, t)
+		// 118
+	case map[string]any:
+		if err := writer.WriteByte(118); err != nil {
+			return err
 		}
-		writter.WriteByte(123)
-		writter.Write(bytes)
-	case float32: // TYPE 124: FLOAT32
-		bits := math.Float32bits(t)
-		bytes := make([]byte, 4)
-		binary.LittleEndian.PutUint32(bytes, bits)
-		if !IsLittleEndian() {
-			bytes = Reverse(bytes)
+		if err := WriteVarUint(writer, uint64(len(t))); err != nil {
+			return err
 		}
-		writter.WriteByte(124)
-		writter.Write(bytes)
-	case int: // TYPE 125: INTEGER
-		writter.WriteByte(125)
-		WriteVarInt(writter, t, false)
-	case int64: // Special case: treat LONG as INTEGER.
-		writter.WriteByte(125)
-		WriteVarInt(writter, int(t), false)
-	case nil: // TYPE 126: null
-		// TYPE 127: undefined
-		writter.WriteByte(126)
-	case []byte: // TYPE 116: ArrayBuffer
-		writter.WriteByte(116)
-		WriteVarUint8Array(writter, t)
-	case map[string]any: // TYPE 118: object (Dictionary<string, object>)
-		writter.WriteByte(118)
-		WriteVarUint(writter, uint32(len(t)))
 		for key, value := range t {
-			WriteVarString(writter, key)
-			WriteAny(writter, value)
+			if err := WriteVarString(writer, key); err != nil {
+				return err
+			}
+			if err := WriteAny(writer, value); err != nil {
+				return err
+			}
 		}
-	case []any: // TYPE 117: Array
-		writter.WriteByte(117)
-		WriteVarUint(writter, uint32(len(t)))
+		// 117
+	case []any:
+		if err := writer.WriteByte(117); err != nil {
+			return err
+		}
+		if err := WriteVarUint(writer, uint64(len(t))); err != nil {
+			return err
+		}
 		for _, item := range t {
-			WriteAny(writter, item)
+			if err := WriteAny(writer, item); err != nil {
+				return err
+			}
 		}
+		// 116
+	case []byte:
+		if err := writer.WriteByte(116); err != nil {
+			return err
+		}
+		WriteUint8Array(writer, t)
 	default:
-		return errors.New(fmt.Sprintf("unsupported type : %v \n", reflect.TypeOf(o)))
+		return errors.New(fmt.Sprintf("unsupported data type : %v \n", reflect.TypeOf(t)))
 	}
-	return errors.New(fmt.Sprintf("unsupported type : %v \n", reflect.TypeOf(o)))
-}
 
-func WriteUint8(writter *bufio.Writer, b byte) error {
-	return writter.WriteByte(b)
-}
-
-func IsLittleEndian() bool {
-	var value int32 = 1 // 占4byte 转换成16进制 0x00 00 00 01
-	// 大端(16进制)：00 00 00 01
-	// 小端(16进制)：01 00 00 00
-	pointer := unsafe.Pointer(&value)
-	pb := (*byte)(pointer)
-	if *pb != 1 {
-		return false
-	}
-	return true
-}
-
-func Reverse(bytes []byte) []byte {
-	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
-		bytes[i], bytes[j] = bytes[j], bytes[i]
-	}
-	return bytes
+	return nil
 }
