5abeb8b5ca1f02d5977df10984cfa16af6b7a42e
Dinesh Salunke <dineshsalunke@consultbop.com>
2024-04-26 14:24:51 +0530

    refactor: implement shared type interface for astract type

--- abstract_type.go
+++ abstract_type.go
@@ -8,7 +8,55 @@ type AbstractType struct {
 	itemMap map[string]*Item
 }
 
+func (at *AbstractType) Doc() *Doc {
+	return at.doc
+}
+
+func (at *AbstractType) Item() *Item {
+	return at.item
+}
+
+func (at *AbstractType) Start() *Item {
+	return at.start
+}
+
+func (at *AbstractType) SetStart(item *Item) {
+	at.start = item
+}
+
+func (at *AbstractType) Length() uint32 {
+	return at.length
+}
+
+func (at *AbstractType) SetLength(length uint32) {
+	at.length = length
+}
+
+func (at *AbstractType) GetItem(key string) *Item {
+	return at.itemMap[key]
+}
+
+func (at *AbstractType) SetItem(key string, item *Item) {
+	at.itemMap[key] = item
+}
+
 func (at *AbstractType) Integrate(doc *Doc, item *Item) {
 	at.doc = doc
 	at.item = item
+}
+
+func (at *AbstractType) Copy() SharedType {
+	return &AbstractType{}
+}
+
+func (at *AbstractType) Clone() SharedType {
+	return &AbstractType{}
+}
+
+func (at *AbstractType) ToJSON() any {
+	return nil
+}
+
+func (at *AbstractType) Write(encoder UpdateEncoder) error {
+	panic("to be implemented by parent structs")
 }
