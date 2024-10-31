f1188ce04d55d42fda82c4dcab0a6bb8a0d6fe10
Dinesh Salunke <dineshsalunke@consultbop.com>
2024-10-31 20:02:10 +0530

    refactor: create the missing place holder methods

--- updates.go
+++ updates.go
@@ -3,3 +3,11 @@ package ynotgo
 func mergeUpdatesV2(updates [][]byte) ([]byte, error) {
 	panic("not implemented")
 }
+
+func diffUpdateV2([]byte, []byte) ([]byte, error) {
+	panic("not implemented")
+}
+
+func mergeUpdates([][]byte) ([]byte, error) {
+	panic("not implemented")
+}
